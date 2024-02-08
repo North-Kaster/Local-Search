6f801a1b07b6f928e9cd135eb489f417e32339cc
jeorozc0 <jeorozco@bsu.edu>
2024-02-08 15:14:35 -0500

    Refactor SimulatedAnnealing class to improve acceptance probability calculation

--- src/core_algorithms/SimulatedAnnealing.java
+++ src/core_algorithms/SimulatedAnnealing.java
@@ -14,26 +14,42 @@ public abstract class SimulatedAnnealing<S> {
 
     private final Problem<S> problem;
 
-    public SimulatedAnnealing(long initTime, double initTemp, Problem<S> p){
+    public SimulatedAnnealing(long initTime, double initTemp, Problem<S> p) {
         this.time = initTime;
         this.temp = initTemp;
         this.problem = p;
     }
 
-    //generate the new (lower) temperature
+    // generate the new (lower) temperature
     public abstract double schedule(long time, double temp);
 
-    public void search(){
+    public void search() {
         /* TODO */
         S state = problem.getInitState();
         while (temp > 0) {
             S newState = problem.generateNewState(state);
             double deltaCost = problem.cost(newState) - problem.cost(state);
+            if (accept(deltaCost, temp)) {
+                state = newState;
+            }
+            time++;
+            temp = schedule(time, temp);
         }
     }
 
-    public boolean accept(double delta, double temp){
+    public boolean accept(double delta, double temp) {
         /* TODO */
+        if (delta < 0) {
+            return true;
+        } else {
+            double probability = Math.exp(-delta / temp);
+            Random r = new Random();
+            if (r.nextDouble() < probability) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
 }
