50da14a4e4aad183d7ec8af1a6e21d799392b3b1
North-Kaster <northkaster@gmail.com>
2024-02-15 14:19:47 -0500

    Finish implementation of Individual class

--- src/core_algorithms/Individual.java
+++ src/core_algorithms/Individual.java
@@ -2,19 +2,16 @@ package core_algorithms;
 
 import java.util.List;
 
-/* <G> Represents the data type of a gene
- */
-public abstract class Individual <G> {
+// <G> Represents the data type of gene
+
+public class Individual <G> implements Comparable<Individual<G>>{
     private List<G> chromosome; // Might want to make these final?
     private double fitnessScore;
 
-    public Individual(List<G> chromosome){
+    public Individual(List<G> chromosome, double fitnessScore){
         this.chromosome = chromosome;
-        this.fitnessScore =
-                calcFitnessScore(this.chromosome);
+        this.fitnessScore = fitnessScore;
     }
-
-    public abstract double calcFitnessScore(List<G> chromosome);
 
     public List<G> getChromosome() {
         return chromosome;
@@ -23,4 +20,17 @@ public abstract class Individual <G> {
     public double getFitnessScore() {
         return fitnessScore;
     }
+
+    @Override
+    public String toString() {
+        return "Individual{" +
+                "chromosome=" + chromosome +
+                ", fitnessScore=" + fitnessScore +
+                '}';
+    }
+
+    public int compareTo(Individual<G> i){
+        return Double.compare(i.getFitnessScore(), this.getFitnessScore());
+    }
 }
+
